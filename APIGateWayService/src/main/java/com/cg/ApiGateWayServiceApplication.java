b5bd89a0c4a4f4d57b9bb6be6abdb16175996728
AkhilKumar199 <102596203+AkhilKumar199@users.noreply.github.com>
2022-05-19 06:45:09 +0530

    ApiGateWayService

--- APIGateWayService/src/main/java/com/cg/ApiGateWayServiceApplication.java
+++ APIGateWayService/src/main/java/com/cg/ApiGateWayServiceApplication.java
@@ -3,10 +3,13 @@ package com.cg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
+//Annotation
 @SpringBootApplication
 @EnableEurekaClient
+//Main Class
 public class ApiGateWayServiceApplication {
+	
+	//Main Mathod
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGateWayServiceApplication.class, args);
