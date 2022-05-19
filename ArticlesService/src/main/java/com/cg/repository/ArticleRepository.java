0d5c93bafc481d5c6d723b6504181ddf585feb40
AkhilKumar199 <102596203+AkhilKumar199@users.noreply.github.com>
2022-05-19 06:55:51 +0530

    Update ArticleRepository.java

--- ArticlesService/src/main/java/com/cg/repository/ArticleRepository.java
+++ ArticlesService/src/main/java/com/cg/repository/ArticleRepository.java
@@ -1,9 +1,11 @@
+// Importing package in this code module
 package com.cg.repository;
-
+//Importing required classes
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.cg.model.Article;
 
+//Interface
 public interface ArticleRepository extends MongoRepository<Article, String>{
 
 }
