b608e1540381ba0c3b68d2b177560bd12d7b7733
AkhilKumar199 <102596203+AkhilKumar199@users.noreply.github.com>
2022-05-20 10:55:57 +0530

    Update AdminContactServiceApplication.java

--- ContactUsService/src/main/java/com/cg/AdminContactServiceApplication.java
+++ ContactUsService/src/main/java/com/cg/AdminContactServiceApplication.java
@@ -1,15 +1,20 @@
+// Importing package in this code module
 package com.cg;
-
+//Importing required classes
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
+//Annotation
 @SpringBootApplication
 @EnableEurekaClient
+
+//Main class
 public class AdminContactServiceApplication {
+	
+	 // Main driver method
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdminContactServiceApplication.class, args);
