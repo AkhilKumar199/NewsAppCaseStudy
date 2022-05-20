191f3d3eda1b78013dbbd559ac3a5cf855e3713f
AkhilKumar199 <102596203+AkhilKumar199@users.noreply.github.com>
2022-05-20 11:07:43 +0530

    Update TechnologyServiceApplication.java

--- TechnologyService/src/main/java/com/cg/TechnologyServiceApplication.java
+++ TechnologyService/src/main/java/com/cg/TechnologyServiceApplication.java
@@ -1,9 +1,11 @@
+// Importing package in this code module
 package com.cg;
-
+//Importing required classes
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //Annotation
 @SpringBootApplication
+//Main class
 public class TechnologyServiceApplication {
 	
 	// Main driver method
