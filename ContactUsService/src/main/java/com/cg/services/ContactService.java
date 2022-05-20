2e81cc5bd83466afc5cb30785c4bd39b949078f7
AkhilKumar199 <102596203+AkhilKumar199@users.noreply.github.com>
2022-05-20 10:52:07 +0530

    Update ContactService.java

--- ContactUsService/src/main/java/com/cg/services/ContactService.java
+++ ContactUsService/src/main/java/com/cg/services/ContactService.java
@@ -1,5 +1,6 @@
+// Importing package module to code fragment
 package com.cg.services;
-
+//Importing required classes
 import java.util.List;
 import java.util.Optional;
 
@@ -12,8 +13,9 @@ import com.cg.repository.ContactRepository;
 
 
 
-
+//Annotation
 @Service
+//Class
 public class ContactService {
 
 	@Autowired
@@ -23,7 +25,7 @@ public class ContactService {
 	public Contact addContact (Contact contact) {
 		return contactrepository.save(contact);
 	}
-
+// Read operation
 	public List<Contact> getContact() {
 		List<Contact> contact = contactrepository.findAll();
 		System.out.println("Getting data from DB : " + contact);
@@ -33,7 +35,7 @@ public class ContactService {
 	public Optional<Contact> getContactbyId(String id) {
 		return contactrepository.findById(id);
 	}
-
+// Delete operation
 	public void deleteContact(Contact contact) {
 		contactrepository.delete(contact);
 	}
