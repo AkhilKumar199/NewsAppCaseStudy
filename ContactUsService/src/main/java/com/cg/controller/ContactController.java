e799a6d4376826c0a538f5dea8ab33ee246460be
AkhilKumar199 <102596203+AkhilKumar199@users.noreply.github.com>
2022-05-20 10:42:12 +0530

    Update ContactController.java

--- ContactUsService/src/main/java/com/cg/controller/ContactController.java
+++ ContactUsService/src/main/java/com/cg/controller/ContactController.java
@@ -1,5 +1,6 @@
+// Importing package in this code module
 package com.cg.controller;
-
+//Importing required classes
 import java.util.List;
 import java.util.Optional;
 
@@ -17,20 +18,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+//Annotation 
 @RestController
 @RequestMapping("/api/v1")
+//Main class
 public class ContactController {
 	
 	@Autowired
 	private ContactRepository contactrepository;
 	
+	// Save operation
 	@PostMapping("/addContact")
 	public String saveBook(@RequestBody Contact contact) {
     contactrepository.save(contact);
 	return "Added contact with id :  " + contact.getId();
 }
 	
+	// Read operation
 	@GetMapping("/findAllContacts")
 	public List<Contact> getContacts(){
 		return contactrepository.findAll();
@@ -41,11 +45,14 @@ public class ContactController {
 		return contactrepository.findById(id);
 	}
 	
+	// Delete operation
 	 @DeleteMapping("/delcontact/{id}")
 		public String deleteContact (@PathVariable String id) {
 			contactrepository.deleteById(id);
 			return "Contact deleted with id : "+id;
 		}
+	
+	// Read operation
 	 @GetMapping("/Contact")
      public Contact getContactData() {
       Contact ct = new Contact ("1","mak","mak@hotmail.com" );
