349af8a24d8ba10a8341ab87c65360660ab10edc
AkhilKumar199 <102596203+AkhilKumar199@users.noreply.github.com>
2022-05-20 10:45:30 +0530

    Update Contact.java

--- ContactUsService/src/main/java/com/cg/model/Contact.java
+++ ContactUsService/src/main/java/com/cg/model/Contact.java
@@ -1,35 +1,39 @@
+// Importing package in this code module
 package com.cg.model;
-
+//Importing required classes
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
+//POJO class
 @Data
 @AllArgsConstructor
 @Document(collection = "Contacts")
 
 public class Contact {
+	//Attributes
 
-   @Id
+   @Id //Primary Key
    private String id;
    
    private String name;
    
    private String email;
 
+//Default Constructor
 public Contact() {
 	super();
 	// TODO Auto-generated constructor stub
 }
-
+//Parameterized Constructor
 public Contact(String id, String name, String email) {
 	super();
 	this.id = id;
 	this.name = name;
 	this.email = email;
 }
+	//getters and setters
 
 public String getId() {
 	return id;
@@ -54,6 +58,8 @@ public String getEmail() {
 public void setEmail(String email) {
 	this.email = email;
 }
+	
+//toString method
 
 @Override
 public String toString() {
