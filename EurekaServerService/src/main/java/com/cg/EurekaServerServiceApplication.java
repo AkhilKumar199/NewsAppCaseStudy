bd8274a0d7f9ea2e2edaeb6af7e23432aa584c33
AkhilKumar199 <102596203+AkhilKumar199@users.noreply.github.com>
2022-05-20 11:12:42 +0530

    Update EurekaServerServiceApplication.java

--- EurekaServerService/src/main/java/com/cg/EurekaServerServiceApplication.java
+++ EurekaServerService/src/main/java/com/cg/EurekaServerServiceApplication.java
@@ -1,12 +1,17 @@
+// Importing package in this code module
 package com.cg;
-
+//Importing required classes
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+//Annotation
 @SpringBootApplication
 @EnableEurekaServer
+//Main class
 public class EurekaServerServiceApplication {
+	
+	// Main driver method
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerServiceApplication.class, args);
