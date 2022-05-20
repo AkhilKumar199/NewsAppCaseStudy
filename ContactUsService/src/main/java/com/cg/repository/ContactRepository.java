c93d08cd3a126e69c4fd7b4c74bf767d1b2767e1
AkhilKumar199 <102596203+AkhilKumar199@users.noreply.github.com>
2022-05-20 10:47:04 +0530

    Update ContactRepository.java

--- ContactUsService/src/main/java/com/cg/repository/ContactRepository.java
+++ ContactUsService/src/main/java/com/cg/repository/ContactRepository.java
@@ -1,9 +1,11 @@
+// Importing package in this code module
 package com.cg.repository;
-
+//Importing required classes
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.cg.model.Contact;
 
+//Interface
 public interface ContactRepository extends MongoRepository<Contact, String> {
 
 
