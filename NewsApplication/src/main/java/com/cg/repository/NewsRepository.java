e613aa9fa0fa3efffdb0e2a737a08d87fe9e8920
AkhilKumar199 <102596203+AkhilKumar199@users.noreply.github.com>
2022-05-20 10:47:31 +0530

    Update NewsRepository.java

--- NewsApplication/src/main/java/com/cg/repository/NewsRepository.java
+++ NewsApplication/src/main/java/com/cg/repository/NewsRepository.java
@@ -1,5 +1,6 @@
+// Importing package in this code module
 package com.cg.repository;
-
+//Importing required classes
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.cg.model.News;
