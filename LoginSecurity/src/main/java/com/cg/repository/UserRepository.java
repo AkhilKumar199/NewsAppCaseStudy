2c1d37efe9c54b0748b4d685741036814c33001e
AkhilKumar199 <102596203+AkhilKumar199@users.noreply.github.com>
2022-05-20 11:10:30 +0530

    Update UserRepository.java

--- LoginSecurity/src/main/java/com/cg/repository/UserRepository.java
+++ LoginSecurity/src/main/java/com/cg/repository/UserRepository.java
@@ -4,8 +4,9 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.cg.model.UserModel;
-
+//Annotation
 @Repository
+//Interface
 public interface UserRepository extends MongoRepository<UserModel, String>{
 	
 	UserModel findByUsername(String username);
