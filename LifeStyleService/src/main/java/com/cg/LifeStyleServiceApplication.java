15b2cff73184edc9db21964c10afb0554a0fca8c
AkhilKumar199 <102596203+AkhilKumar199@users.noreply.github.com>
2022-05-20 11:06:53 +0530

    Update LifeStyleServiceApplication.java

--- LifeStyleService/src/main/java/com/cg/LifeStyleServiceApplication.java
+++ LifeStyleService/src/main/java/com/cg/LifeStyleServiceApplication.java
@@ -1,12 +1,17 @@
+// Importing package in this code module
 package com.cg;
-
+//Importing required classes
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+//Annotation
 @SpringBootApplication
 @EnableEurekaClient
+//Main class
 public class LifeStyleServiceApplication {
+	
+	// Main driver method
 
 	public static void main(String[] args) {
 		SpringApplication.run(LifeStyleServiceApplication.class, args);
